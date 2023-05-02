a6e0d9879e4b4ab05c78b7a8729a63f0033b0776
cwd.systems <101006007+infinitydaemon@users.noreply.github.com>
2023-05-02 09:10:46 +0500

    Update server.go
    * Attempt to make the code modular and easy to understand.

--- cmd/cloudflared/tunnel/server.go
+++ cmd/cloudflared/tunnel/server.go
@@ -10,28 +10,75 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+const (
+	proxyDNSPortKey             = "proxy-dns-port"
+	proxyDNSMaxUpstreamConnsKey = "proxy-dns-max-upstream-conns"
+	proxyDNSAddressKey          = "proxy-dns-address"
+	proxyDNSUpstreamKey         = "proxy-dns-upstream"
+	proxyDNSBootstrapKey        = "proxy-dns-bootstrap"
+)
+
 func runDNSProxyServer(c *cli.Context, dnsReadySignal chan struct{}, shutdownC <-chan struct{}, log *zerolog.Logger) error {
-	port := c.Int("proxy-dns-port")
+	port := c.Int(proxyDNSPortKey)
 	if port <= 0 || port > 65535 {
 		return errors.New("The 'proxy-dns-port' must be a valid port number in <1, 65535> range.")
 	}
-	maxUpstreamConnections := c.Int("proxy-dns-max-upstream-conns")
+
+	maxUpstreamConnections := c.Int(proxyDNSMaxUpstreamConnsKey)
 	if maxUpstreamConnections < 0 {
-		return fmt.Errorf("'%s' must be 0 or higher", "proxy-dns-max-upstream-conns")
+		return fmt.Errorf("'%s' must be 0 or higher", proxyDNSMaxUpstreamConnsKey)
 	}
-	listener, err := tunneldns.CreateListener(c.String("proxy-dns-address"), uint16(port), c.StringSlice("proxy-dns-upstream"), c.StringSlice("proxy-dns-bootstrap"), maxUpstreamConnections, log)
+
+	listener, err := createDNSListener(c, port, maxUpstreamConnections, log)
 	if err != nil {
 		close(dnsReadySignal)
-		listener.Stop()
 		return errors.Wrap(err, "Cannot create the DNS over HTTPS proxy server")
 	}
 
-	err = listener.Start(dnsReadySignal)
+	err = startDNSListener(listener, dnsReadySignal)
 	if err != nil {
 		return errors.Wrap(err, "Cannot start the DNS over HTTPS proxy server")
 	}
-	<-shutdownC
-	_ = listener.Stop()
+
+	waitForShutdown(shutdownC)
+
+	return stopDNSListener(listener, log)
+}
+
+func createDNSListener(c *cli.Context, port, maxUpstreamConnections int, log *zerolog.Logger) (tunneldns.Listener, error) {
+	listener, err := tunneldns.CreateListener(
+		c.String(proxyDNSAddressKey),
+		uint16(port),
+		c.StringSlice(proxyDNSUpstreamKey),
+		c.StringSlice(proxyDNSBootstrapKey),
+		maxUpstreamConnections,
+		log,
+	)
+
+	return listener, err
+}
+
+func startDNSListener(listener tunneldns.Listener, dnsReadySignal chan struct{}) error {
+	return listener.Start(dnsReadySignal)
+}
+
+func stopDNSListener(listener tunneldns.Listener, log *zerolog.Logger) error {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Warn().Msg("Error stopping DNS listener: %v", r)
+		}
+	}()
+
+	err := listener.Stop()
+	if err != nil {
+		return errors.Wrap(err, "Error stopping DNS listener")
+	}
+
 	log.Info().Msg("DNS server stopped")
+
 	return nil
+}
+
+func waitForShutdown(shutdownC <-chan struct{}) {
+	<-shutdownC
 }
